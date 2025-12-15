bbdb5891c109201326f81cc42233d81a4637fcce
WellMess <240692815+WellMess@users.noreply.github.com>
2025-12-15 09:16:53 -0500

    fix: Corrige a associação das imagens de capa (thumbnails) com os temas ambientais

--- constants.ts
+++ constants.ts
@@ -1,4 +1,3 @@
-
 import { ThemesData } from './types';
 
 export const THEMES_DATA: ThemesData = {
@@ -6,8 +5,8 @@ export const THEMES_DATA: ThemesData = {
         title: 'Impactos dos Resíduos no Ambiente',
         icon: 'fa-trash-alt',
         color: 'text-red-600',
-        image: '/images/impactos_residuos_ambiente.png', // Imagem completa (PÁGINA 5 do PDF anterior)
-        thumbnailImage: '/images/thumbnails/capa_tema1_residuos.png', // Capa (PÁGINA 5 do novo PDF)
+        image: '/images/impactos_residuos_ambiente.png',
+        thumbnailImage: '/images/thumbnails/capa_tema3_estuarios.png', // Imagem da página 2
         quote: 'Imagem do tema: Impactos dos Resíduos no Ambiente',
         content: ''
     },
@@ -15,8 +14,8 @@ export const THEMES_DATA: ThemesData = {
         title: 'Preservação das Restingas',
         icon: 'fa-mountain',
         color: 'text-yellow-700',
-        image: '/images/preservacao_restingas.png', // Imagem completa (PÁGINA 7 do PDF anterior)
-        thumbnailImage: '/images/thumbnails/capa_tema2_restingas.png', // Capa (PÁGINA 7 do novo PDF)
+        image: '/images/preservacao_restingas.png',
+        thumbnailImage: '/images/thumbnails/capa_tema6_conchas.png', // Imagem da página 3
         quote: 'Imagem do tema: Preservação das Restingas',
         content: ''
     },
@@ -24,8 +23,8 @@ export const THEMES_DATA: ThemesData = {
         title: 'Preservação dos Estuários e Manguezais',
         icon: 'fa-water',
         color: 'text-green-700',
-        image: '/images/preservacao_estuarios_manguezais.png', // Imagem completa (PÁGINA 2 do PDF anterior)
-        thumbnailImage: '/images/thumbnails/capa_tema3_estuarios.png', // Capa (PÁGINA 2 do novo PDF)
+        image: '/images/preservacao_estuarios_manguezais.png',
+        thumbnailImage: '/images/thumbnails/capa_tema4_caranguejo.png', // Imagem da página 4
         quote: 'Imagem do tema: Preservação dos Estuários e Manguezais',
         content: ''
     },
@@ -33,8 +32,8 @@ export const THEMES_DATA: ThemesData = {
         title: 'Conservação do Caranguejo-Uçá',
         icon: 'fa-spider',
         color: 'text-orange-700',
-        image: '/images/conservacao_caranguejo_uca.png', // Imagem completa (PÁGINA 4 do PDF anterior)
-        thumbnailImage: '/images/thumbnails/capa_tema4_caranguejo.png', // Capa (PÁGINA 4 do novo PDF)
+        image: '/images/conservacao_caranguejo_uca.png',
+        thumbnailImage: '/images/thumbnails/capa_tema1_residuos.png', // Imagem da página 5
         quote: 'Imagem do tema: Conservação do Caranguejo-Uçá',
         content: ''
     },
@@ -42,8 +41,8 @@ export const THEMES_DATA: ThemesData = {
         title: 'Proteção dos Eolianitos',
         icon: 'fa-gem',
         color: 'text-purple-700',
-        image: '/images/protecao_eolianitos.png', // Imagem completa (PÁGINA 6 do PDF anterior)
-        thumbnailImage: '/images/thumbnails/capa_tema5_eolianitos.png', // Capa (PÁGINA 6 do novo PDF)
+        image: '/images/protecao_eolianitos.png',
+        thumbnailImage: '/images/thumbnails/capa_tema5_eolianitos.png', // Imagem da página 6
         quote: 'Imagem do tema: Proteção dos Eolianitos',
         content: ''
     },
@@ -51,8 +50,8 @@ export const THEMES_DATA: ThemesData = {
         title: 'Combate ao Hábito de Levar Conchas e Animais',
         icon: 'fa-fish',
         color: 'text-teal-700',
-        image: '/images/combate_levar_conchas_animais.png', // Imagem completa (PÁGINA 3 do PDF anterior)
-        thumbnailImage: '/images/thumbnails/capa_tema6_conchas.png', // Capa (PÁGINA 3 do novo PDF)
+        image: '/images/combate_levar_conchas_animais.png',
+        thumbnailImage: '/images/thumbnails/capa_tema2_restingas.png', // Imagem da página 7
         quote: 'Imagem do tema: Combate ao Hábito de Levar Conchas e Animais',
         content: ''
     },
@@ -60,8 +59,8 @@ export const THEMES_DATA: ThemesData = {
         title: 'NÃO COMPRE PESCADOS PROIBIDOS',
         icon: 'fa-ban',
         color: 'text-red-800',
-        image: '/images/novo_tema_nao_compre_pescados_proibidos.png', // Imagem completa (PÁGINA 1 do PDF anterior)
-        thumbnailImage: '/images/thumbnails/capa_tema7_nao_compre.png', // Capa (PÁGINA 1 do novo PDF)
+        image: '/images/novo_tema_nao_compre_pescados_proibidos.png',
+        thumbnailImage: '/images/thumbnails/capa_tema7_nao_compre.png', // Imagem da página 1
         quote: 'Imagem do tema: NÃO COMPRE PESCADOS PROIBIDOS',
         content: ''
     }
